0e5138774ccfb29b1958699f7a0e31eec6882663
Edwin Steele <edwin-lists@wordspeak.org>
2014-12-15 14:55:16 +1100

    Argumentificate the departure window and clean-up the output

--- train_notify.py
+++ train_notify.py
@@ -1,3 +1,4 @@
+import argparse
 from datetime import datetime, timedelta
 import itertools
 import requests
@@ -7,7 +8,7 @@ BASE_URL = "http://realtime.grofsoft.com/tripview/realtime?routes=%s&type=dtva"
 
 
 class Trip(object):
-    def __init__(self, trip_id):
+    def __init__(self, trip_id, first_departure_time, last_departure_time):
         self.trip_id = trip_id
         self.start_time_str = "unknown"
         self.start_time_timedelta = timedelta.max
@@ -16,6 +17,8 @@ class Trip(object):
         self.location = "unknown"
         self.alert = None
         self.offset_tuples = []
+        self.first_departure_time = first_departure_time
+        self.last_departure_time = last_departure_time
 
     def populate_estimated_arrival_times(self):
         start_time = self.start_time_timedelta
@@ -25,28 +28,27 @@ class Trip(object):
         min_transit, max_transit = conf.transit_times.get(
             self.start_loc_str,
             (timedelta.min, timedelta.max - timedelta(days=1)))
-        self.est_arrival_earliest = start_time + min_transit
-        self.est_arrival_latest = start_time + max_transit
+        self.est_scheduled_arrival_earliest = start_time + min_transit
+        self.est_scheduled_arrival_latest = start_time + max_transit
 
     def is_current(self):
         """Arrives at departure station in the future"""
-        n = datetime.now()
-        return timedelta(hours=n.hour, minutes=n.minute) \
-            < self.est_arrival_latest
+        return now_as_timedelta() < self.est_scheduled_arrival_latest
 
     def arrives_in_departure_window(self):
         """Arrives at departure station in the window"""
         return \
-            self.est_arrival_earliest > conf.FIRST_DEPARTURE_TIME and \
-            self.est_arrival_latest < conf.LAST_DEPARTURE_TIME
+            (self.first_departure_time <
+             self.est_scheduled_arrival_earliest <
+             self.last_departure_time) or \
+            (self.first_departure_time <
+             self.est_scheduled_arrival_latest <
+             self.last_departure_time)
 
     def estimate_delay_at_boarding_station(self):
         possible_delay_tuples = list(itertools.dropwhile(
-            lambda x: x[0] < self.est_arrival_earliest, self.offset_tuples))
-        #print "Est Arrival: %s - %s, Possible delays: %s" %\
-        #      (self.est_arrival_earliest,
-        #       self.est_arrival_latest,
-        #       list(possible_delay_tuples),)
+            lambda x: x[0] < self.est_scheduled_arrival_earliest,
+            self.offset_tuples))
         # The first delay number will be close enough, so let's use that
         if possible_delay_tuples:
             return possible_delay_tuples[0][1]
@@ -66,11 +68,13 @@ class Trip(object):
         if not (self.is_current() and self.arrives_in_departure_window()):
             s = ""
         else:
-            s = "%s from %s (%s). Currently at %s." % \
-                (self.start_time_str,
-                self.start_loc_str,
-                self.delay_description(),
-                self.location)
+            s = "%s: Scheduled arrival: %s-%s currently at %s (%s from %s)." % \
+                (self.delay_description(),
+                 self.est_scheduled_arrival_earliest,
+                 self.est_scheduled_arrival_latest,
+                 self.location,
+                 self.start_time_str,
+                 self.start_loc_str)
             if self.alert:
                 s += " Alert: %s" % (self.alert,)
         return s
@@ -82,18 +86,31 @@ class Trip(object):
         if not self.is_current():
             s += "[In the past] "
 
-        s += "Trip %s. %s from %s (%s). Currently at %s. Alert: %s" % \
+        s += "Trip %s. %s from %s (%s). Currently at %s." \
+             " Estimated arrival: %s-%s. Alert: %s" % \
             (self.trip_id,
              self.start_time_str,
              self.start_loc_str,
              self.delay_description(),
              self.location,
+             self.est_scheduled_arrival_earliest,
+             self.est_scheduled_arrival_latest,
              self.alert)
         return s
 
 
-def extract_trip(j, trip_id):
-    t = Trip(trip_id)
+def hhmm_string_to_timedelta(s):
+    """hh:mm to timedelta"""
+    return timedelta(0, 0, 0, 0, *map(int, reversed(s.split(":"))))
+
+
+def now_as_timedelta():
+    n = datetime.now()
+    return timedelta(hours=n.hour, minutes=n.minute)
+
+
+def extract_trip(j, trip_id, fdt, ldt):
+    t = Trip(trip_id, fdt, ldt)
     delay_data = filter(lambda x: x["tripId"] == trip_id, j["delays"])
     #transposition_data = filter(
     #    lambda x: x["tripId"] == trip_id, j["transpositions"])
@@ -103,8 +120,8 @@ def extract_trip(j, trip_id):
     if delay_data:
         t.start_time_str = delay_data[0]["start"]
         # "hh:mm" to timedelta
-        t.start_time_timedelta = timedelta(
-            0, 0, 0, 0, *map(int, reversed(delay_data[0]["start"].split(":"))))
+        t.start_time_timedelta = \
+            hhmm_string_to_timedelta(delay_data[0]["start"])
         t.start_loc_int = int(delay_data[0]["stopId"])
         t.start_loc_str = conf.stop_ids.get(
             t.start_loc_int, "Unknown")
@@ -118,7 +135,7 @@ def extract_trip(j, trip_id):
         else:
             offsets_raw_list = []
         t.offset_tuples = zip(
-            [timedelta(0, 0, 0, 0, *map(int, reversed(x.split(":"))))
+            [hhmm_string_to_timedelta(x)
              for x in (itertools.islice(offsets_raw_list, 0, None, 2))],
             map(int, itertools.islice(offsets_raw_list, 1, None, 2))
         )
@@ -126,35 +143,58 @@ def extract_trip(j, trip_id):
     if vehicle_data:
         t.location = vehicle_data[0]["lp"].rsplit(":", 2)[0]
     if alert_data:
-        t.alert = alert_data[0]["title"]
+        if "body" in alert_data[0]:
+            t.alert = alert_data[0]["body"]
+        else:
+            t.alert = alert_data[0]["title"]
 
     t.populate_estimated_arrival_times()
     return t
 
 
-
-if __name__ == "__main__":
+def main(fdt, ldt):
     r = requests.get(BASE_URL % (conf.ROUTES,))
     j = r.json()
 
     print "Retrieved at: %s" % \
           (datetime.fromtimestamp(j["timestamp"]).ctime(),)
+    print "Looking for arrivals between %s and %s" % (fdt, ldt)
 
     # Save the realtime data for troubleshooting and verification
     with open("/Users/esteele/realtime.json", "w") as f:
         f.write(r.text)
 
     # Trains only appear in the vehicles list once they have actually departed.
-    # Trains that are past their departure time ("start") but have not left their
-    #  origin are not listed in vehicles until they've actually left the station.
+    # Trains that are past their departure time ("start") but have not left
+    #  their origin are not listed in vehicles until they've actually left
+    #  the station.
     trips = []
-    for tripId in [v["tripId"] for v in j["vehicles"] if v["route"] == conf.ROUTES]:
-        trips.append(extract_trip(j, tripId))
+    for tripId in [v["tripId"] for v in j["vehicles"]
+                   if v["route"] == conf.ROUTES]:
+        trips.append(extract_trip(j, tripId, fdt, ldt))
 
-    print "------"
+    print "--- Short summary start ---"
     for t in trips:
         if t.is_current() and t.arrives_in_departure_window():
             print t.short_summary()
-    print "------"
+    print "--- Short summary end ---"
     for t in trips:
         print t.full_summary()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--first_departure_time")
+    parser.add_argument("--last_departure_time")
+    args = parser.parse_args()
+    if args.first_departure_time:
+        first_departure_time = \
+            hhmm_string_to_timedelta(args.first_departure_time)
+    else:
+        first_departure_time = now_as_timedelta()
+    if args.last_departure_time:
+        last_departure_time = \
+            hhmm_string_to_timedelta(args.last_departure_time)
+    else:
+        last_departure_time = now_as_timedelta() + timedelta(minutes=60)
+
+    main(first_departure_time, last_departure_time)
