f2e2e842fdcf7007a41b2d33194738fa8ec7e5b3
Edwin Steele <edwin-lists@wordspeak.org>
2015-03-29 15:38:39 +1100

    Use constants for colours

--- notifier.py
+++ notifier.py
@@ -4,6 +4,9 @@ import conf
 from phue import Bridge
 from pushover import Client
 
+GREEN = 20389
+RED = 65535
+
 
 def send_pushover_notification(message, title):
     client = Client(conf.PUSHOVER_USER, api_token=conf.PUSHOVER_API_TOKEN)
@@ -15,8 +18,8 @@ def set_lamp_state(is_late):
     b.set_light(conf.HUE_LIGHT_NAME, 'on', True)  # Make sure it's on
     b.set_light(conf.HUE_LIGHT_NAME, 'bri', 254)  # Max brightness
     if is_late:
-        b.set_light(conf.HUE_LIGHT_NAME, 'hue', 65535)
+        b.set_light(conf.HUE_LIGHT_NAME, 'hue', RED)
     else:
-        b.set_light(conf.HUE_LIGHT_NAME, 'hue', 20389)
+        b.set_light(conf.HUE_LIGHT_NAME, 'hue', GREEN)
     # Only stay on for 30 seconds
     b.set_light(conf.HUE_LIGHT_NAME, 'on', False, transitiontime=300)
